1bcc214a9b368a78ddddcee7643b71d021098f9e
httpteruel <20230006488@estudantes.ifpr.edu.br>
2026-04-15 10:05:41 -0300

    test: deve rejeitar senha com menos de 8 caracteres

--- tests/test_validator.py
+++ tests/test_validator.py
@@ -3,3 +3,9 @@ from src.validator import is_strong_password
 
 def test_should_reject_password_less_than_8_chars():
     assert is_strong_password("Ab1!def") is False
+
+def test_should_reject_password_without_uppercase():
+    assert is_strong_password("lowercas1!") is False
+
+def test_should_reject_password_without_lowercase():
+    assert is_strong_password("UPPERCAS1!") is False
