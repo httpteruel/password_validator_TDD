6b3fadbaade86ade46d1368aacbd2f51c1676794
httpteruel <20230006488@estudantes.ifpr.edu.br>
2026-04-15 10:06:45 -0300

    feat: adiciona validação de letras maiúsculas e minúsculas

--- src/validator.py
+++ src/validator.py
@@ -1,4 +1,8 @@
 def is_strong_password(password: str) -> bool:
     if len(password) < 8:
         return False
+    if not any(c.isupper() for c in password):
+        return False
+    if not any(c.islower() for c in password):
+        return False
     return True
